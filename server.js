afe2bf811fa1bac07142d4b70c74e38065e63f8b
Jalil <agoumi.jalil@gmail.com>
2018-05-26 19:00:23 +0200

    maj

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ let express = require("express")
 app = express()
 
 app.use('/static',express.static('static'))
+app.set('view engine','jade')
 
 app.get("/",(req,res)=>{
-	res.sendFile(__dirname+"/views/pages/index.html")
+	// res.sendFile(__dirname+"/views/pages/index.html")
+		res.render('pages/index',{test:"salut"})
 })
 
 app.listen(8080)
